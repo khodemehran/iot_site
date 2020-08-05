6e10b65d5686f9c472975bb527eeed67017e6038
mehran <mehranbidkhori@gmail.com>
2020-08-05 15:32:48 +0430

    some changes for watch our models

--- iot_server/data/models.py
+++ iot_server/data/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Data(models.model):
+class Data(models.Model):
+
+    rotobat = models.FloatField(max_length=3)
+    dama = models.FloatField(max_length=3)
+    date = models.DateTimeField()
+    tozih = models.CharField(max_length=300)
+    
+
+
