b981bbb180bda3913adfd4699547794d05da575a
mehran <mehranbidkhori@gmail.com>
2020-08-06 16:10:20 +0430

    files change

--- iot_server/data/models.py
+++ iot_server/data/models.py
@@ -3,10 +3,16 @@ from django.db import models
 # Create your models here.
 class Data(models.Model):
 
-    rotobat = models.FloatField(max_length=3)
-    dama = models.FloatField(max_length=3)
+    humidity = models.FloatField(max_length=3)
+    temp = models.FloatField(max_length=3)
     date = models.DateTimeField()
     tozih = models.CharField(max_length=300)
-    
+
+
+
+    def __str__(self):
+        return self.tozih
+
+
 
 
