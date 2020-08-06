4263c15c3c738df64cc5966aa7e62d31e117b359
mehran <mehranbidkhori@gmail.com>
2020-08-06 20:44:17 +0430

    changes created

--- iot_server/data/views.py
+++ iot_server/data/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Data
 
 def home(request):
-
-    return HttpResponse('home now')
+    all_data = Data.objects.order_by('-date')
+    return render(request, 'data/home.html',{'all_data':all_data})
